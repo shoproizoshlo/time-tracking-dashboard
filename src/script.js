5c9a3cefeceed80a9b76648460f8675a07c5c7e7
shoproizoshlo <iamsusunny@gmail.com>
2023-10-06 12:38:43 +0200

    created script for fetch json data

--- src/script.js
+++ src/script.js
@@ -1,27 +1,29 @@
-fetch("./src/data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((item, index) => {
-      //   console.log(item);
-      if (item.title === "Work") {
-        console.log("piska");
-        console.log(item.timeframes.daily.current);
-        console.log(item.timeframes.daily.previous);
-      }
-    });
-  })
-  .catch((error) => console.error("Error loading data:", error));
-
 const periodRanges = document.querySelectorAll(".period-range");
+const lastWeekHours = document.getElementById("last-week-hours");
+const cardType = document.getElementById("work"); // Update with appropriate card identifier
+
+const fetchData = async () => {
+  try {
+    const response = await fetch("./src/data.json"); // Adjust the path accordingly
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
 
 periodRanges.forEach((period) => {
-  period.addEventListener("click", () => {
-    // Remove the "active" class from all elements
-    periodRanges.forEach((element) => {
-      element.classList.remove("active");
-    });
+  period.addEventListener("click", async () => {
+    const selectedPeriod = period.id;
+    const jsonData = await fetchData();
 
-    // Add the "active" class to the clicked element
-    period.classList.add("active");
+    jsonData.forEach((cardData) => {
+      const data = cardData.timeframes[selectedPeriod];
+      const cardTitle = cardData.title;
+
+      if (cardType.id === cardTitle.toLowerCase()) {
+        lastWeekHours.textContent = `${data.current}hrs`;
+      }
+    });
   });
 });
