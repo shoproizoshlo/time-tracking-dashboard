490ae1174ce6d3e646ea0955d653387ae2560bec
shoproizoshlo <iamsusunny@gmail.com>
2023-10-06 16:31:33 +0200

    fixed  updating data

--- src/script.js
+++ src/script.js
@@ -1,59 +1,8 @@
-// const periodRanges = document.querySelectorAll(".period-range");
-// const lastWeekHours = document.getElementById("last-week-hours");
-// const currentWeekHours = document.getElementById("current-week-hours");
-// const lastPeriod = document.getElementById("last-period");
-// const cardType = document.getElementById("work"); // Update with appropriate card identifier
-
-// const fetchData = async () => {
-//   try {
-//     const response = await fetch("./src/data.json"); // Adjust the path accordingly
-//     const jsonData = await response.json();
-//     return jsonData;
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//   }
-// };
-
-// periodRanges.forEach((period) => {
-//   period.addEventListener("click", async () => {
-//     // Remove the "active" class from all elements
-//     periodRanges.forEach((element) => {
-//       element.classList.remove("active");
-//     });
-
-//     const selectedPeriod = period.id;
-
-//     //   change name of the period
-//     if (selectedPeriod === "daily") {
-//       lastPeriod.textContent = `Day`;
-//     } else if (selectedPeriod === "weekly") {
-//       lastPeriod.textContent = `Week`;
-//     }
-//     if (selectedPeriod === "monthly") {
-//       lastPeriod.textContent = `Month`;
-//     }
-
-//     const jsonData = await fetchData();
-//     //   change data hours base in JSON file
-
-//     jsonData.forEach((cardData) => {
-//       const data = cardData.timeframes[selectedPeriod];
-//       const cardTitle = cardData.title;
-//       if (cardType.id === cardTitle.toLowerCase()) {
-//         currentWeekHours.textContent = `${data.current}hrs`;
-//         lastWeekHours.textContent = `${data.previous}hrs`;
-//       }
-//     });
-
-//     // Add the "active" class to the clicked element
-//     period.classList.add("active");
-//   });
-// });
-
 const periodRanges = document.querySelectorAll(".period-range");
 const lastWeekHours = document.querySelectorAll(".last-week-hours");
 const currentWeekHours = document.querySelectorAll(".hrs");
 const cardTypes = document.querySelectorAll(".card-type");
+const lastPeriod = document.querySelectorAll(".last-period");
 
 const fetchData = async () => {
   try {
@@ -78,8 +27,24 @@ const updateCardData = async (selectedPeriod, cardId) => {
 };
 
 periodRanges.forEach((period) => {
+  //   updateCardData(period.id, "work");
   period.addEventListener("click", async () => {
+    //   Remove the "active" class from all elements
+    periodRanges.forEach((element) => {
+      element.classList.remove("active");
+    });
+    // Add the "active" class to the clicked element
+    period.classList.add("active");
     const selectedPeriod = period.id;
+
+    //   change name of the period
+    if (selectedPeriod === "daily") {
+      lastPeriod.textContent = `Day`;
+    } else if (selectedPeriod === "weekly") {
+      lastPeriod.textContent = `Week`;
+    } else if (selectedPeriod === "monthly") {
+      lastPeriod.textContent = `Month`;
+    }
 
     cardTypes.forEach((card) => {
       const cardId = card.id;
@@ -87,3 +52,16 @@ periodRanges.forEach((period) => {
     });
   });
 });
+
+const initPage = () => {
+  // Add the "active" class to the clicked element
+  const selectedPeriod = periodRanges[1];
+  selectedPeriod.classList.add("active");
+
+  cardTypes.forEach((card) => {
+    const cardId = card.id;
+    updateCardData("weekly", cardId);
+  });
+};
+
+initPage();
